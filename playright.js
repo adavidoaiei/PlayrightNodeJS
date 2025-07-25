17a42c8df54551dcdf241211cf9e45449233ae40
adavidoaiei <cornel.adavidoaiei@gmail.com>
2025-07-25 14:42:38 +0300

    fix

--- playright.js
+++ playright.js
@@ -1,7 +1,7 @@
 const { chromium } = require('playwright');
 
 (async () => {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://litoralulromanesc.ro');
 
